950a5ef7f1cf48709c323e4c58a5a041c7197c5a
sam <908172357@qq.com>
2020-06-09 20:39:31 +1200

    added the test components

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <span>Driving</span> <span style="font-weight: bolder">Test</span>
+                    <span>Driving</span>
+                    <span style="font-weight: bolder">Test</span>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
