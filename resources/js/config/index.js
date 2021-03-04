a210517c5894547e8f130ec04595935829201377
Sam <sam.kiwi2019@gmail.com>
2021-03-04 20:01:50 +1300

    update config for docker tasks

--- resources/js/config/index.js
+++ resources/js/config/index.js
@@ -3,7 +3,7 @@ export default {
      * @description api请求基础路径
      */
     baseUrl: {
-        dev: 'http://127.0.0.1:8000',
+        dev: 'https://driving.keenneed.com',
         pro: 'https://driving.keenneed.com',
     },
 };
