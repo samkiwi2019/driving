8e22670049616cf6a7106dc911cc55c573b94565
Sam <sam.kiwi2019@gmail.com>
2021-03-05 01:59:39 +1300

    testing mysql 8.0+

--- resources/js/config/index.js
+++ resources/js/config/index.js
@@ -3,7 +3,7 @@ export default {
      * @description api请求基础路径
      */
     baseUrl: {
-        dev: 'http://localhost:9001',
+        dev: 'http://localhost:8000',
         pro: 'https://driving.keenneed.com',
     },
 };
